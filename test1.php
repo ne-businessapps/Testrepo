865fad1b8c8443c79207254841767694ab8e6481
ne-businessapps <support@businessapps.co.in>
2018-01-22 17:12:49 +0530

    added at 5:15PM

--- test1.php
+++ test1.php
@@ -1,7 +1,8 @@
 <?php 
 
-    echo "Test4 File.... 1st message!";
+    echo "Test4 File.... 1ST message!";
 
-
+	echo "Test4 File.... 2ND message!";
+	echo "Test4 File.... 3RD message!";
 
 ?>
--- test4.php
+++ test4.php
@@ -1,10 +1,6 @@
 <?php
+  echo "Test4 File.... 1st  message!";
   echo "Test4 File.... 2nd message!";
-  echo "Test4 File.... 3rd message!";
-
-	echo "Test4 File.... 3rd message!";
-        echo "Test4 File.... 4rd message!";
-        echo "Test4 File.... 5rd message!";
 
 
 
